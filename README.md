d5cbf50a7cf4a614a16e3ac691f8abff46687b92
starstruckman <164056551+starstruckman@users.noreply.github.com>
2024-03-20 03:49:51 +1100

    mmm
    m

---
+++ README.md
@@ -0,0 +1,17 @@
+# web3-101-checkandget
+
+Check and get started with technologies, exploring the basics and practical applications.
+
+## Installation
+
+```bash
+git clone https://github.com/starstruckman/web3-101-checkandget.git
+cd web3-101-checkandget
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+const web3_101_checkandget = require('web3-101-checkandget');
+const web3_101_checkandget_implement = require('web3-101-checkandget-implement');
+const underscore = require('underscore');
+const webpack_cli = require('webpack-cli');
+const bcrypt = require('bcrypt');
+const fs_extra = require('fs-extra');
+const supertest = require('supertest');
+
+const fs = require('fs');
+const readableStream = fs.createReadStream('input.txt');
+const writableStream = fs.createWriteStream('output.txt');
+readableStream.pipe(writableStream);
+console.log('File copied using streams');
+
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+setTimeoutPromise(2000, 'done').then(value => {
+  console.log(value);
+});
+
+process.on('exit', (code) => {
+  console.log(`About to exit with code: ${code}`);
+});
+process.exit();
